ad7b05fe4dd59fca37c882323fe0d8d7ee930476
Choijiwoong <gogogi313@naver.com>
2022-10-05 19:07:35 +0900

    [DOCS] 스켈레톤 파일 업로드

---
+++ src/main/java/com/holly/molly/MollyApplication.java
@@ -0,0 +1,13 @@
+package com.holly.molly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MollyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MollyApplication.class, args);
+	}
+
+}

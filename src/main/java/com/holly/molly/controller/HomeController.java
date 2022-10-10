edf108d57efa374a62a413bcf2be9b6f1fbb62fc
Choijiwoong <gogogi313@naver.com>
2022-10-11 01:45:05 +0900

    [FEAT] html template적용 시도, js 미작동 디버깅 필요

--- src/main/java/com/holly/molly/controller/HomeController.java
+++ src/main/java/com/holly/molly/controller/HomeController.java
@@ -9,5 +9,5 @@ import com.holly.molly.domain.Member;
 @Controller
 public class HomeController {
     @GetMapping("/")//지정X 시 index.html로 이동. 우선순위가 자바 컨테이너에서 먼저 찾기에 현재의 컨트롤러 매핑을 인지
-    public String home(){ return "home"; }
+    public String home(){ return "home_template/home"; }
 }

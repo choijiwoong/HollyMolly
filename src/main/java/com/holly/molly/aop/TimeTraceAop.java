af4391cb9eaff4b5eda30599f57cc1c6af7b3a46
Choijiwoong <gogogi313@naver.com>
2022-10-13 21:15:44 +0900

    [DOCS] 서버 재시작 시 DB 초기화 오류 해결(create-table)

---
+++ src/main/java/com/holly/molly/aop/TimeTraceAop.java
@@ -0,0 +1,24 @@
+package com.holly.molly.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeTraceAop {
+
+    @Around("execution(* com.holly.molly..*(..))")//molly.service..로 변경시 해당 부분만 AOP가 적용되게 수정이 가능
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable{
+        long start=System.currentTimeMillis();
+        System.out.println("START: "+joinPoint.toString());
+        try{
+            return joinPoint.proceed();//프록시라는 가짜 서비스를 실행하여 시간을 체킹 뒤에 실제 작업
+        } finally{
+            long finish=System.currentTimeMillis();
+            long timeMs=finish-start;
+            System.out.println("END: "+joinPoint.toString()+" "+timeMs+"ms");
+        }
+    }
+}

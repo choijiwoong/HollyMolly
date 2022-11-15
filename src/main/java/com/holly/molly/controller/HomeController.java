e683b9893a8f717004c8a1f61d744124dae3d544
Choijiwoong <gogogi313@naver.com>
2022-11-15 16:27:34 +0900

    [FEAT] 로그인 시 Cookie이용 userId 기록. 추후 세션을 이용한 업그레이드

--- src/main/java/com/holly/molly/controller/HomeController.java
+++ src/main/java/com/holly/molly/controller/HomeController.java
@@ -1,13 +1,33 @@
 package com.holly.molly.controller;
 
+import com.holly.molly.domain.User;
+import com.holly.molly.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final UserService userService;
     @GetMapping("/")//지정X 시 index.html로 이동. 우선순위가 자바 컨테이너에서 먼저 찾기에 현재의 컨트롤러 매핑을 인지
-    public String home(){ return "homeTemplate/home"; }
+    public String home(Model model, @CookieValue(value="userId", required = false) Cookie cookie){
+        if(cookie!=null){
+            System.out.println("Exist Cookie");
+            model.addAttribute("user", Optional.of(userService.findOne(Long.valueOf(cookie.getValue()))));//cookie의 string(id)를 integer로
+        } else{
+            System.out.println("NotExist Cookie");
+            model.addAttribute("user", Optional.empty());
+        }
+        return "homeTemplate/home";
+    }
 
     @GetMapping("/introducePage")//지정X 시 index.html로 이동. 우선순위가 자바 컨테이너에서 먼저 찾기에 현재의 컨트롤러 매핑을 인지
     public String intro(){ return "homeTemplate/post/firstStep"; }
---
+++ src/main/java/com/holly/molly/controller/SessionConstants.java
@@ -0,0 +1,5 @@
+package com.holly.molly.controller;
+
+public interface SessionConstants {//객체 생성을 막기 위해 interface
+    String LOGIN_MEMBER="loginmember";//HttpSession에서 로그인용으로 사용할 세션 ID
+}

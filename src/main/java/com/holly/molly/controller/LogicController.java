0bd836ab092193283b61409277d090ab182a4682
Choijiwoong <gogogi313@naver.com>
2022-11-25 22:18:46 +0900

    [DOCS] makeComplete 디버깅완료

--- src/main/java/com/holly/molly/controller/LogicController.java
+++ src/main/java/com/holly/molly/controller/LogicController.java
@@ -122,8 +122,10 @@ public class LogicController {
         return "volun/detailRequest";
     }
 
+    @Transactional
     @GetMapping("request/makeComplete/{requestid}")
     public String makeRequestComplete(@PathVariable("requestid") Long requestId){
+        System.out.println("[DEBUG] CALL MAKEREQUESTCOMPLETE");
         Request request=requestService.findOne(requestId);
         if(request.getStatus()!=RequestStatus.ACCEPT){
             throw new RuntimeException("Request의 상태가 Accept가 아닙니다!");
@@ -134,14 +136,16 @@ public class LogicController {
             throw new RuntimeException("Accept의 상태가 Register가 아닙니다!");
         }
 
-        request.setStatus(RequestStatus.COMPLETE);
         accept.setStatus(AcceptStatus.COMPLETE);
+        request.setStatus(RequestStatus.COMPLETE);
 
         return "redirect:/";
     }
 
+    @Transactional
     @GetMapping("accept/makeComplete/{acceptid}")
     public String makeAcceptComplete(@PathVariable("acceptid") Long acceptId){
+        System.out.println("[DEBUG] CALL MAKEACCEPTCOMPLETE");
         Accept accept=acceptService.findOne(acceptId);
         if(accept.getStatus()!=AcceptStatus.REGISTER){
             throw new RuntimeException("Accept의 상태가 Register가 아닙니다!");
@@ -152,8 +156,8 @@ public class LogicController {
             throw new RuntimeException("Request의 상태가 Accept가 아닙니다!");
         }
 
-        request.setStatus(RequestStatus.COMPLETE);
         accept.setStatus(AcceptStatus.COMPLETE);
+        request.setStatus(RequestStatus.COMPLETE);
 
         return "redirect:/";
     }
--- src/main/java/com/holly/molly/service/AcceptService.java
+++ src/main/java/com/holly/molly/service/AcceptService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 

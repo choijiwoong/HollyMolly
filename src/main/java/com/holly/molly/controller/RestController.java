4e36d057f5a46ec6f3ae6cf9cb9a10de6d163e5f
최지웅 <gogogi313@naver.com>
2023-09-22 12:54:25 +0900

    [FEAT?DEBUG] 후...RestAPI기능구현 틀을 드디어 해냈네.. script에서도 jquery를 추가해야한다는 점이 관건. 지금은 테스트코드고 추후에 RestFul하게 리팩토링하며 지울 예정

--- src/main/java/com/holly/molly/controller/RestController.java
+++ src/main/java/com/holly/molly/controller/RestController.java
@@ -1,10 +1,11 @@
 package com.holly.molly.controller;
 
+import com.holly.molly.DTO.ReviewDTO;
+import com.holly.molly.VO.TestVO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {//ajax를 이용한 Rest API방식 컨트롤러. 개발중.
@@ -12,6 +13,18 @@ public class RestController {//ajax를 이용한 Rest API방식 컨트롤러. �
     public ResponseEntity<String> ajaxTest(){
         System.out.println("********work*******");
         return new ResponseEntity<>(HttpStatus.OK);
+    }//postman으로 http://localhost:8080/ajax시 정상적으로 1이 반환됨.
+
+    @RequestMapping (value="/rest/test1", method=RequestMethod.POST)
+    public ResponseEntity<String> test1(@RequestBody TestVO testVO) throws Exception{
+        System.out.println("[RESULT]"+ testVO.getName());
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @RequestMapping(value="/rest/test2", method=RequestMethod.GET)
+    public TestVO test2(){
+        TestVO testVO=new TestVO();
+        testVO.setName("hello");
+        return testVO;
+    }
 }
